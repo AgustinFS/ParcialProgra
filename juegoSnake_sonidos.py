e4940d412ca137b744545a5c6f4495e27388d270
AgustinFS12 <AgustinFS12@hotmail.com>
2024-12-03 21:59:09 -0300

    Actualizando archivos parte 3

--- juegoSnake_sonidos.py
+++ juegoSnake_sonidos.py
@@ -1,20 +1,20 @@
 import pygame
 
-# Control de sonido
+#Control de sonido
 sound_enabled = True
 
-# Función para cargar y reproducir sonidos
+#Función para cargar y reproducir sonidos
 def play_sound(file):
     global sound_enabled
     if sound_enabled:
         pygame.mixer.music.load(file)
         pygame.mixer.music.play()
 
-# Cambiar estado de sonido
+#Cambiar estado de sonido
 def toggle_sound():
     global sound_enabled
     sound_enabled = not sound_enabled
 
-# Verificar estado del sonido
+#Verificar estado del sonido
 def is_sound_enabled():
     return sound_enabled
